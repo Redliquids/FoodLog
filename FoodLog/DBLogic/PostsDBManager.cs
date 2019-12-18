4ed17db564962338c96bb5bcd749d2326319107a
Victor S <victor.skarerverket@gmail.com>
2019-12-18 13:15:37 +0100

    Added SQLite NuGet

---
+++ FoodLog/DBLogic/PostsDBManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace FoodLog.DBLogic
+{
+    public class PostsDBManager
+    {
+        public static string conString = "Data Source=C:\\FoodLogDB\\FoodLogDB.db";
+
+
+    }
+}
