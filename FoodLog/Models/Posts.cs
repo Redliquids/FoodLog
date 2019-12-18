0c15b6d57821c720b1714e4542847ea154dfcf27
Victor S <victor.skarerverket@gmail.com>
2019-12-18 12:42:31 +0100

    Added Posts model
    Added Posts model and some requirements to the variables.

---
+++ FoodLog/Models/Posts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using System.Linq;
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodLog.Models
+{
+    public class Posts
+    {
+        [Key]
+        public int postID { get; set; }
+
+        [Required]
+        public DateTime posted { get; set; }
+
+        [Required]
+        //[StringLength(maximumLength: 120, MinimumLength = 2, ErrorMessage = "* Please Enter a Ingredient. 2 - 40 Letters")]
+        public string notes { get; set; }
+    }
+}
