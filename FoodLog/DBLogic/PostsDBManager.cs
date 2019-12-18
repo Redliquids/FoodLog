757d92d48f3a5851d5567b9d18289bf311f31cc9
Victor S <victor.skarerverket@gmail.com>
2019-12-18 15:08:28 +0100

    Worked on DB Connection
    Checking and creating directory and file for DB

--- FoodLog/DBLogic/PostsDBManager.cs
+++ FoodLog/DBLogic/PostsDBManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using System.IO;
 using System.Data.SQLite;
 
 namespace FoodLog.DBLogic
@@ -10,6 +12,24 @@ namespace FoodLog.DBLogic
     {
         public static string conString = "Data Source=C:\\FoodLogDB\\FoodLogDB.db";
 
+        public static void SetupDB()
+        {
+            // If DB Directory doesn't exist, Create it.
+            if (!Directory.Exists("C:\\FoodLogDB"))
+            {
+                Directory.CreateDirectory("C:\\FoodLogDB");
+            }
 
+            if (!File.Exists("C:\\FoodLogDB\\FoodLogDB.db"))
+            {
+                SQLiteConnection.CreateFile("C:\\FoodLogDB\\FoodLogDB.db");
+
+                using (SQLiteConnection con = new SQLiteConnection(conString))
+                {
+
+                }
+            }
+
+        }
     }
 }
